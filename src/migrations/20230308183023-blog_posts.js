6b9f8fba147a7d78923a3788799797560de2acb2
Amanda Portela <mandaptela@gmail.com>
2023-03-08 16:13:34 -0300

    Requisito 1

---
+++ src/migrations/20230308183023-blog_posts.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('blog_posts', { 
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+        type:Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      userId: {
+        field: 'user_id',
+        reference: {
+          model: 'users',
+          key: 'id'
+        },
+        type: Sequelize.INTEGER
+      },
+      published: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('blog_posts');
+  }
+};
